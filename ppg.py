ae991c8f1d71d3e685c630dbdc35ab6d8adcacb4
Jonathan W. Cranford <jonathan.w.cranford@gmail.com>
2021-05-20 09:35:33 -0400

    added phrase_count argument

--- ppg.py
+++ ppg.py
@@ -10,41 +10,15 @@ parser.add_argument("-w", "--word_count",
     help="number of words in the passphrase (default is 5)",
     default=5,
     type=int)
-
-# def usage():
-#     print(f"{sys.argv[0]} generates a passphrase from the given file of the")
-#     print('given number of words.')
-#     print()
-#     print(f"Usage: {sys.argv[0]} [-w word_count] file")
-#     print("where")
-#     print('  file           is a word list, one word per line')
-#     print('  word_count     is the number of words in the passphrase ')
-#     print('                 (default value: 5)')
-
-# if (len(sys.argv) < 2):
-#     usage()
-#     sys.exit(1)
+parser.add_argument("-n", "--phrase_count",
+    help="number of passphrases to generate (default is 20)",
+    default=20,
+    type=int)
 
 args = parser.parse_args()
-
-# argi = 1
-# word_count = 5
-# if (len(sys.argv) <= 4 and '-w' == sys.argv[argi]):
-#     argi += 1
-#     try:
-#         word_count = int(sys.argv[argi])
-#     except ValueError:
-#         print(f"Invalid word count: {sys.argv[argi]}")
-#         print()
-#         usage()
-#         sys.exit(2)
-#     argi += 1
-
-
-# word_file = sys.argv[argi]
 
 with open(args.file) as f:
     words = [word.strip() for word in f]
 
-password = ' '.join(secrets.choice(words) for i in range(args.word_count))
-print(password)
+for n in range(args.phrase_count):
+    print(' '.join(secrets.choice(words) for i in range(args.word_count))) 
