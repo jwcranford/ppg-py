613f9cc8cae57eb8cb57d6d83b70b84ed5d51f4c
Jonathan W. Cranford <jonathan.w.cranford@gmail.com>
2021-05-20 09:30:11 -0400

    converted to argparse

--- ppg.py
+++ ppg.py
@@ -2,38 +2,49 @@
 
 import secrets
 import sys
+import argparse
 
-def usage():
-    print(f"{sys.argv[0]} generates a passphrase from the given file of the")
-    print('given number of words.')
-    print()
-    print(f"Usage: {sys.argv[0]} [-w word_count] file")
-    print("where")
-    print('  file           is a word list, one word per line')
-    print('  word_count     is the number of words in the passphrase ')
-    print('                 (default value: 5)')
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help='word list, one word per line')
+parser.add_argument("-w", "--word_count", 
+    help="number of words in the passphrase (default is 5)",
+    default=5,
+    type=int)
 
-if (len(sys.argv) < 2):
-    usage()
-    sys.exit(1)
+# def usage():
+#     print(f"{sys.argv[0]} generates a passphrase from the given file of the")
+#     print('given number of words.')
+#     print()
+#     print(f"Usage: {sys.argv[0]} [-w word_count] file")
+#     print("where")
+#     print('  file           is a word list, one word per line')
+#     print('  word_count     is the number of words in the passphrase ')
+#     print('                 (default value: 5)')
 
-argi = 1
-word_count = 5
-if (len(sys.argv) <= 4 and '-w' == sys.argv[argi]):
-    argi += 1
-    try:
-        word_count = int(sys.argv[argi])
-    except ValueError:
-        print(f"Invalid word count: {sys.argv[argi]}")
-        print()
-        usage()
-        sys.exit(2)
-    argi += 1
+# if (len(sys.argv) < 2):
+#     usage()
+#     sys.exit(1)
 
-word_file = sys.argv[argi]
+args = parser.parse_args()
 
-with open(word_file) as f:
+# argi = 1
+# word_count = 5
+# if (len(sys.argv) <= 4 and '-w' == sys.argv[argi]):
+#     argi += 1
+#     try:
+#         word_count = int(sys.argv[argi])
+#     except ValueError:
+#         print(f"Invalid word count: {sys.argv[argi]}")
+#         print()
+#         usage()
+#         sys.exit(2)
+#     argi += 1
+
+
+# word_file = sys.argv[argi]
+
+with open(args.file) as f:
     words = [word.strip() for word in f]
 
-password = ' '.join(secrets.choice(words) for i in range(word_count))
+password = ' '.join(secrets.choice(words) for i in range(args.word_count))
 print(password)
